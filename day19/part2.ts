2326b9a99d74766eb8a9dcad36bd34ecca88b03e
Valentin <valentin.rogister@gmail.com>
2023-03-28 14:04:27 +0200

    not sure why but it works

--- day19/part2.ts
+++ day19/part2.ts
@@ -9,30 +9,12 @@ const m = data.split("\n").reduce((m, x) => {
   return m.set(k, v);
 }, new Map<string, string>());
 
-let res = Infinity;
-const cache = new Map<string, number>();
-function compute(mol: string, n = 0) {
-  if (n >= res) {
-    return;
+function compute(s: string): number {
+  if (s === "e") {
+    return 0;
   }
-  if (cache.has(mol) && n >= cache.get(mol)!) {
-    return;
-  }
-  cache.set(mol, n);
-  if (mol === "e") {
-    res = Math.min(res, n);
-    console.log(res)
-    Deno.exit()
-    return;
-  }
-  for (const [k, v] of m) {
-    let i = 0;
-    while (mol.indexOf(k, i) !== -1) {
-      compute(mol.substring(0, i) + mol.substring(i).replace(k, v), n + 1);
-      i = mol.indexOf(k, i) + 1;
-    }
-  }
+  const [k, v] = [...m.entries()].find(([k, _]) => s.includes(k))!;
+  return 1 + compute(s.replace(k, v));
 }
 
-compute(mol);
-console.log(res);
+console.log(compute(mol));
